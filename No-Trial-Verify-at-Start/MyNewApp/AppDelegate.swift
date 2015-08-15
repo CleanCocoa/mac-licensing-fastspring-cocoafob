672269bf788032b94c99262ffed5635d35f960dc
Christian Tietze <christian.tietze@gmail.com>
2015-08-15 15:25:06 +0200

    remove cruft

--- No-Trial-Verify-at-Start/MyNewApp/AppDelegate.swift
+++ No-Trial-Verify-at-Start/MyNewApp/AppDelegate.swift
@@ -1,27 +1,13 @@
-//
-//  AppDelegate.swift
-//  MyNewApp
-//
-//  Created by Christian Tietze on 15/08/15.
-//  Copyright (c) 2015 Christian Tietze. All rights reserved.
-//
-
 import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
-
-
+    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        
+        
     }
-
-    func applicationWillTerminate(aNotification: NSNotification) {
-        // Insert code here to tear down your application
-    }
-
-
 }
 
