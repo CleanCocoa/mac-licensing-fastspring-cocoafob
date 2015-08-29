146b2471122b22cf0c3086657d05286e256f5189
Christian Tietze <christian.tietze@gmail.com>
2015-08-29 14:57:58 +0200

    add bind() and >>= operator to shared code

--- Shared/Optional.swift
+++ Shared/Optional.swift
@@ -10,3 +10,26 @@ func hasValue<T>(value: T?) -> Bool {
     case .None: return false
     }
 }
+
+/// Super-useful as `>>=` operator to chain function 
+/// calls which take optionals:
+///     
+///     foo() >>= bar >>= baz
+///
+/// `.None` will cascade, `.Some(_:T)` will be passed 
+/// on to the next in chain.
+func bind<T, U>(optional: T?, f: T -> U?) -> U? {
+    
+    if let x = optional {
+        return f(x)
+    } else {
+        return .None
+    }
+}
+
+infix operator >>= {  associativity left precedence 150 }
+
+func >>=<T, U>(optional: T?, f: T -> U?) -> U? {
+    
+    return bind(optional, f)
+}
