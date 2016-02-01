9c8dc33fa5d299a483dada02b3427794525c9a0c
Christian Tietze <christian.tietze@gmail.com>
2016-02-01 16:12:29 +0100

    update Me.swift to make address book lookup failable

--- No-Trial-Verify-at-Start/MyNewApp/Me.swift
+++ No-Trial-Verify-at-Start/MyNewApp/Me.swift
@@ -8,70 +8,92 @@ import AddressBook
 /// Wrapper around `ABPerson` to expose properties used by the FastSpring store
 /// in a useful manner with proper types.
 class Me {
-    
-    let person: ABPerson
-    
+
+    let person: ABPerson?
+
     convenience init() {
-        
-        self.init(person: ABAddressBook.sharedAddressBook().me())
+
+        // Force-cast to optional to not crash when rights are missing.
+        self.init(person: ABAddressBook.sharedAddressBook()?.me())
     }
-    
-    init(person: ABPerson) {
-        
+
+    /// - parameter persion: Pass `nil` for unknown data.
+    init(person: ABPerson?) {
+
         self.person = person
     }
-    
+
     static let Unknown = ""
-    
+
     var firstName: String {
-        
-        if let firstName = person.valueForProperty(kABFirstNameProperty) as? String {
-            
-            return firstName
+
+        guard let person = person else {
+            return Me.Unknown
         }
-        
-        return Me.Unknown
+
+        guard let firstName = person.valueForProperty(kABFirstNameProperty) as? String else {
+
+            return Me.Unknown
+        }
+
+        return firstName
     }
-    
+
     var lastName: String {
-        
-        if let lastName = person.valueForProperty(kABLastNameProperty) as? String {
-            
-            return lastName
+
+        guard let person = person else {
+            return Me.Unknown
         }
-        
-        return Me.Unknown
+
+        guard let lastName = person.valueForProperty(kABLastNameProperty) as? String else {
+
+            return Me.Unknown
+        }
+
+        return lastName
     }
-    
+
     var organization: String {
-        
-        if let lastName = person.valueForProperty(kABLastNameProperty) as? String {
-            
-            return lastName
+
+        guard let person = person else {
+            return Me.Unknown
         }
-        
-        return Me.Unknown
+
+        guard let lastName = person.valueForProperty(kABLastNameProperty) as? String else {
+
+            return Me.Unknown
+        }
+
+        return lastName
     }
-    
+
     var primaryEmail: String {
-        
-        if let allEmails = person.valueForProperty(kABEmailProperty) as? ABMultiValue,
-            primaryEmail = allEmails.valueAtIndex(allEmails.indexForIdentifier(allEmails.primaryIdentifier())) as? String {
-                
-                return primaryEmail
+
+        guard let person = person else {
+            return Me.Unknown
         }
-        
-        return Me.Unknown
+
+        guard let allEmails = person.valueForProperty(kABEmailProperty) as? ABMultiValue,
+            primaryEmail = allEmails.valueAtIndex(allEmails.indexForIdentifier(allEmails.primaryIdentifier())) as? String else {
+
+                return Me.Unknown
+        }
+
+        return primaryEmail
     }
-    
+
     var primaryPhone: String {
-        
-        if let allPhones = person.valueForProperty(kABPhoneProperty) as? ABMultiValue,
-            primaryPhone = allPhones.valueAtIndex(allPhones.indexForIdentifier(allPhones.primaryIdentifier())) as? String {
+
+        guard let person = person else {
+            return Me.Unknown
+        }
+
+        guard let allPhones = person.valueForProperty(kABPhoneProperty) as? ABMultiValue,
+            primaryPhone = allPhones.valueAtIndex(allPhones.indexForIdentifier(allPhones.primaryIdentifier())) as? String else {
                 
-                return primaryPhone
+                return Me.Unknown
         }
         
-        return Me.Unknown
+        return primaryPhone
     }
 }
