5cd72033dfee079676f49b692bd56109b3c41c1a
Christian Tietze <christian.tietze@gmail.com>
2019-09-06 13:08:23 +0200

    cleanup StoreController

--- No-Trial-Verify-at-Start/MyNewApp/StoreController.swift
+++ No-Trial-Verify-at-Start/MyNewApp/StoreController.swift
@@ -1,148 +1,142 @@
 // Copyright (c) 2015-2019 Christian Tietze
-// 
+//
 // See the file LICENSE for copying permission.
 
 import Foundation
 
 public class StoreController: NSObject {
-    
+
     var storeDelegate: StoreDelegate?
     var orderConfirmationView: OrderConfirmationView?
 
     // Expose to @objc for Key--Value-Coding
     @objc let storeController: FsprgEmbeddedStoreController
     let storeInfo: StoreInfo
-    
+
     public init(storeInfo: StoreInfo) {
-        
+
         self.storeController = FsprgEmbeddedStoreController()
         self.storeInfo = storeInfo
-        
+
         super.init()
-        
+
         storeController.setDelegate(self)
     }
-    
+
     public func loadStore() {
-        
+
         storeController.load(with: storeParameters)
     }
-    
+
     var storeParameters: FsprgStoreParameters {
-        
+
         let storeParameters = FsprgStoreParameters()
-        
+
         // Set up store to display the correct product
         storeParameters?.setOrderProcessType(kFsprgOrderProcessDetail)
         storeParameters?.setStoreId(self.storeInfo.storeId,
-            withProductId: self.storeInfo.productId)
+                                    withProductId: self.storeInfo.productId)
         storeParameters?.setMode(self.storeInfo.storeMode)
-        
+
         // Pre-populate form fields with personal contact details
         let me = Me()
-        
+
         storeParameters?.setContactFname(me.firstName)
         storeParameters?.setContactLname(me.lastName)
         storeParameters?.setContactCompany(me.organization)
-        
+
         storeParameters?.setContactEmail(me.primaryEmail)
         storeParameters?.setContactPhone(me.primaryPhone)
-        
+
         return storeParameters!
     }
-    
-    
+
+
     // MARK: Forwarding to FsprgEmbeddedStoreController
-    
+
     public func setWebView(_ webView: WebView) {
-        
+
         storeController.setWebView(webView)
     }
 }
 
 extension StoreController: FsprgEmbeddedStoreDelegate {
-    
+
     public func webView(_ sender: WebView!, didFailProvisionalLoadWithError error: Error!, for frame: WebFrame!) {
     }
-    
+
     public func webView(_ sender: WebView!, didFailLoadWithError error: Error!, for frame: WebFrame!) {
     }
-    
+
     public func didLoadStore(_ url: URL!) {
     }
-    
+
     public func didLoadPage(_ url: URL!, of pageType: FsprgPageType) {
     }
-    
+
     // MARK: Order receiced
-    
+
     public func didReceive(_ order: FsprgOrder!) {
-        
+
         // Thanks Obj-C bridging without nullability annotations:
         // implicit unwrapped optionals are not safe
-        if !hasValue(order) {
-            return
-        }
-        
-        guard let license = licenseFromOrder(order) else {
-            return
-        }
-        
+        guard let order = order,
+            let license = licenseFromOrder(order)
+            else { return }
+
         storeDelegate?.didPurchase(license: license)
     }
-    
+
     fileprivate func licenseFromOrder(_ order: FsprgOrder) -> License? {
-        
-        guard  let items = order.orderItems() as? [FsprgOrderItem],
+
+        guard let items = order.orderItems() as? [FsprgOrderItem],
             let license = items
                 .filter(orderItemIsForThisApp)
                 .map(licenseFromOrderItem) // -> [License?]
                 .filter(hasValue)          // keep non-nil
                 .map({ $0! })              // -> [License]
                 .first else {
-            
+
             return nil
         }
-        
+
         return license
     }
-    
+
     fileprivate func orderItemIsForThisApp(_ orderItem: FsprgOrderItem) -> Bool {
-        
-        let appName = storeInfo.productName
-        
+
         guard let productName = orderItem.productName() else {
             return false
         }
-        
+
+        let appName = storeInfo.productName
         return productName.hasPrefix(appName)
     }
-    
+
     fileprivate func licenseFromOrderItem(_ orderItem: FsprgOrderItem) -> License? {
-        
+
         guard let orderLicense = orderItem.license(),
             let name = orderLicense.licenseName(),
             let licenseCode = orderLicense.firstLicenseCode() else {
-            
+
             return nil
         }
-        
+
         return License(name: name, licenseCode: licenseCode)
     }
-    
-    
+
+
     // MARK: Thank-you view
-    
+
     public func view(withFrame frame: NSRect, for order: FsprgOrder!) -> NSView! {
-        
-        guard  let orderConfirmationView = orderConfirmationView,
+
+        guard let orderConfirmationView = orderConfirmationView,
             let license = licenseFromOrder(order) else {
-            
+
             return nil
         }
-        
+
         orderConfirmationView.displayLicenseCode(license.licenseCode)
-        
         return orderConfirmationView
     }
 }
--- Trial-Expire-While-Running/MyNewApp/StoreController.swift
+++ Trial-Expire-While-Running/MyNewApp/StoreController.swift
@@ -1,146 +1,142 @@
 // Copyright (c) 2015-2019 Christian Tietze
-// 
+//
 // See the file LICENSE for copying permission.
 
 import Foundation
 
 public class StoreController: NSObject {
-    
+
     var storeDelegate: StoreDelegate?
     var orderConfirmationView: OrderConfirmationView?
 
     // Expose to @objc for Key--Value-Coding
     @objc let storeController: FsprgEmbeddedStoreController
     let storeInfo: StoreInfo
-    
+
     public init(storeInfo: StoreInfo) {
-        
+
         self.storeController = FsprgEmbeddedStoreController()
         self.storeInfo = storeInfo
-        
+
         super.init()
-        
+
         storeController.setDelegate(self)
     }
-    
+
     public func loadStore() {
-        
+
         storeController.load(with: storeParameters)
     }
-    
+
     var storeParameters: FsprgStoreParameters {
-        
+
         let storeParameters = FsprgStoreParameters()
-        
+
         // Set up store to display the correct product
         storeParameters?.setOrderProcessType(kFsprgOrderProcessDetail)
         storeParameters?.setStoreId(self.storeInfo.storeId,
-            withProductId: self.storeInfo.productId)
+                                    withProductId: self.storeInfo.productId)
         storeParameters?.setMode(self.storeInfo.storeMode)
-        
+
         // Pre-populate form fields with personal contact details
         let me = Me()
-        
+
         storeParameters?.setContactFname(me.firstName)
         storeParameters?.setContactLname(me.lastName)
         storeParameters?.setContactCompany(me.organization)
-        
+
         storeParameters?.setContactEmail(me.primaryEmail)
         storeParameters?.setContactPhone(me.primaryPhone)
-        
+
         return storeParameters!
     }
-    
-    
+
+
     // MARK: Forwarding to FsprgEmbeddedStoreController
-    
-    public func set(webView: WebView) {
-        
+
+    public func setWebView(_ webView: WebView) {
+
         storeController.setWebView(webView)
     }
 }
 
 extension StoreController: FsprgEmbeddedStoreDelegate {
-    
+
     public func webView(_ sender: WebView!, didFailProvisionalLoadWithError error: Error!, for frame: WebFrame!) {
     }
-    
+
     public func webView(_ sender: WebView!, didFailLoadWithError error: Error!, for frame: WebFrame!) {
     }
-    
+
     public func didLoadStore(_ url: URL!) {
     }
-    
+
     public func didLoadPage(_ url: URL!, of pageType: FsprgPageType) {
     }
-    
+
     // MARK: Order receiced
-    
+
     public func didReceive(_ order: FsprgOrder!) {
-        
+
         // Thanks Obj-C bridging without nullability annotations:
         // implicit unwrapped optionals are not safe
-        if !hasValue(order) {
-            return
-        }
-        
-        if let license = license(fromOrder: order) {
-            storeDelegate?.didPurchase(license: license)
-        }
+        guard let order = order,
+            let license = licenseFromOrder(order)
+            else { return }
+
+        storeDelegate?.didPurchase(license: license)
     }
-    
-    fileprivate func license(fromOrder order: FsprgOrder) -> License? {
-        
-        if let items = order.orderItems() as? [FsprgOrderItem],
+
+    fileprivate func licenseFromOrder(_ order: FsprgOrder) -> License? {
+
+        guard let items = order.orderItems() as? [FsprgOrderItem],
             let license = items
                 .filter(orderItemIsForThisApp)
-                .map(license(fromOrderItem:)) // -> [License?]
-                .filter(hasValue)             // keep non-nil
-                .map({ $0! })                 // -> [License]
-                .first {
-            
-            return license
+                .map(licenseFromOrderItem) // -> [License?]
+                .filter(hasValue)          // keep non-nil
+                .map({ $0! })              // -> [License]
+                .first else {
+
+            return nil
         }
-        
-        return nil
+
+        return license
     }
-    
-    fileprivate func orderItemIsForThisApp(orderItem: FsprgOrderItem) -> Bool {
-        
+
+    fileprivate func orderItemIsForThisApp(_ orderItem: FsprgOrderItem) -> Bool {
+
+        guard let productName = orderItem.productName() else {
+            return false
+        }
+
         let appName = storeInfo.productName
-        
-        if let productName = orderItem.productName() {
-            return productName.hasPrefix(appName)
-        }
-        
-        return false
+        return productName.hasPrefix(appName)
     }
-    
-    fileprivate func license(fromOrderItem orderItem: FsprgOrderItem) -> License? {
-        
-        if let orderLicense = orderItem.license(),
+
+    fileprivate func licenseFromOrderItem(_ orderItem: FsprgOrderItem) -> License? {
+
+        guard let orderLicense = orderItem.license(),
             let name = orderLicense.licenseName(),
-            let licenseCode = orderLicense.firstLicenseCode() {
-                
-            return License(name: name, licenseCode: licenseCode)
+            let licenseCode = orderLicense.firstLicenseCode() else {
+
+            return nil
         }
-        
-        return nil
+
+        return License(name: name, licenseCode: licenseCode)
     }
-    
-    
+
+
     // MARK: Thank-you view
-    
+
     public func view(withFrame frame: NSRect, for order: FsprgOrder!) -> NSView! {
-        
-        if let orderConfirmationView = orderConfirmationView,
-            let license = license(fromOrder: order) {
-            
-            orderConfirmationView.displayLicenseCode(licenseCode: license.licenseCode)
-            
-            return orderConfirmationView
+
+        guard let orderConfirmationView = orderConfirmationView,
+            let license = licenseFromOrder(order) else {
+
+            return nil
         }
-        
-        return nil
+
+        orderConfirmationView.displayLicenseCode(license.licenseCode)
+        return orderConfirmationView
     }
 }
